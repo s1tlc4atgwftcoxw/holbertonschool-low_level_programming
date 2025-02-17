a69fc5a71dc91be1bb733c61ed8299c220eff889
s1tlc4atgwftcoxw <197543012+s1tlc4atgwftcoxw@users.noreply.github.com>
2025-02-17 05:00:41 +0000

    task-2-7: fix edgecase

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
 /**
+ * _abs - Return the absolute value of an integer
+ * @n: The number to return the absolute value of
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
+
+/**
  * print_last_digit - Print the last digit of a number
  * @n: The number to print the last digit of
  *
@@ -8,7 +19,7 @@
  */
 int print_last_digit(int n)
 {
-	int digit = (n < 0 ? -n : n) % 10;
+	int digit = _abs(n % 10);
 
 	_putchar('0' + digit);
 
